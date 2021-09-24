96284a89551229dbb65b0694895f87f0b4fa0a8b
dineshveer <dveer123@hotmail.com>
2021-09-24 14:06:42 +0530

    Difference between trim and strip

--- src/com/dinesh/stringChanges/Example.java
+++ src/com/dinesh/stringChanges/Example.java
@@ -12,58 +12,57 @@ public class Example {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	
-		//isBlank method
-		String string1 = "";
-        System.out.println(string1.isBlank());
-  
-        String string2 = "Testcontent";
-        System.out.println(string2.isBlank());
-        
-      //Output:
-      //True
-      //False
-        
-        //lines method to get collection structure to manupulate it
-        String lines = "Dinesh\nVeer";
-        System.out.println(lines
-                               .lines()
-                               .collect(
-                                   Collectors.toList()));
-        
-        //Output:
-        //[Dinesh, Veer]
-        
-        
-        //repeat repeats number of times given string
-        
-        String sample = "SampleToRepeat";
-        System.out.println(sample.repeat(4));
-        
-        //output
-        //SampleToRepeatSampleToRepeatSampleToRepeatSampleToRepeat
-        
-        
-        //stripLeading -- removed leading white spaces
-        String str = "   leading spave--";
-        System.out.println(str.stripLeading());
-        //output:
-        //leading spave--
-        
-        //stripTraling --removes white spaces from end
-        String strTrail = "--trailing space     ";
-        System.out.println(strTrail.stripTrailing());
-        //output
-        //--trailing space
 
-        
-        //strip -- removes white spaces from both end front and back
-        String bothFrontBack = "    Frontback white spaces      ";
-        System.out.println(bothFrontBack.strip());
-        
-        //output
-        //Frontback white spaces
+		// isBlank method
+		String string1 = "";
+		System.out.println(string1.isBlank());
+
+		String string2 = "Testcontent";
+		System.out.println(string2.isBlank());
+
+		// Output:
+		// True
+		// False
+
+		// lines method to get collection structure to manupulate it
+		String lines = "Dinesh\nVeer";
+		System.out.println(lines.lines().collect(Collectors.toList()));
+
+		// Output:
+		// [Dinesh, Veer]
+
+		// repeat repeats number of times given string
+
+		String sample = "SampleToRepeat";
+		System.out.println(sample.repeat(4));
+
+		// output
+		// SampleToRepeatSampleToRepeatSampleToRepeatSampleToRepeat
+
+		// stripLeading -- removed leading white spaces
+		String str = "   leading spave--";
+		System.out.println(str.stripLeading());
+		// output:
+		// leading spave--
+
+		// stripTraling --removes white spaces from end
+		String strTrail = "--trailing space     ";
+		System.out.println(strTrail.stripTrailing());
+		// output
+		// --trailing space
+
+		// strip -- removes white spaces from both end front and back
+		String bothFrontBack = "    Frontback white spaces      ";
+		System.out.println(bothFrontBack.strip());
+
+		// output
+		// Frontback white spaces
+
+		// Note: The existing trim() method provides the same functions as the strip()
+		// method with the only difference being the white-space definition. In the
+		// trim() method, the white-space is defined as any character whose codepoint is
+		// less than or equal to \u0020. In the strip() method, the white-space is
+		// defined by the static Character.isWhitespace method.
 	}
 
 }
-
