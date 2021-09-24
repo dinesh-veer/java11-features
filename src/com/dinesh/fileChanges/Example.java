fde03ee65e5be4d74ca4194f9e745a0a47239ed1
dineshveer <dveer123@hotmail.com>
2021-09-24 14:19:04 +0530

    updated code

--- src/com/dinesh/fileChanges/Example.java
+++ src/com/dinesh/fileChanges/Example.java
@@ -13,6 +13,11 @@ public class Example {
 			
 			//reads data from file if file not present then NoSuchFileException will thrown		
 			System.out.println(Files.readString(Path.of("text.txt")));
+			
+			//used to locate the path locates the same file or not
+			System.out.println("Path is same ot not : "+Files.isSameFile(Path.of("text.txt"), Path.of("text.txt")));
+			System.out.println("Path is same ot not : "+Files.isSameFile(Path.of("text.txt"), Path.of("text2.txt")));
+			
 		}catch(IOException ex) {
 			System.err.println("Exception occured --"+ex);
 		}
