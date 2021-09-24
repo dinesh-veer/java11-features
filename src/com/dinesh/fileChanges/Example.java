decc170ea69eae8be7dbc0c6781150aabfeec04d
dineshveer <dveer123@hotmail.com>
2021-09-24 14:14:45 +0530

    Added file related new methods

---
+++ src/com/dinesh/fileChanges/Example.java
@@ -0,0 +1,20 @@
+package com.dinesh.fileChanges;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Example {
+
+	public static void main(String[] args) {
+		try {
+			//create a file if it is not present
+			Files.writeString(Path.of("text.txt"), "Data to be written in file");
+			
+			//reads data from file if file not present then NoSuchFileException will thrown		
+			System.out.println(Files.readString(Path.of("text.txt")));
+		}catch(IOException ex) {
+			System.err.println("Exception occured --"+ex);
+		}
+	}
+}
